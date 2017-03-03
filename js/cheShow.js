f18b10a090beb950d4b1c632b4a0eab67952895c
niexiaobo <flash_lifing@163.com>
2017-03-03 16:36:17 +0800

    add
    test

---
+++ js/cheShow.js
@@ -0,0 +1,62 @@
+function init() {
+	requestData();
+}
+//发送请求
+function requestData(){
+	
+$.getJSON("http://oa.mgogo.com/chefu/interface/get_two_type_home.php",
+  {
+    cateid:"1",
+    subid:"8"
+  },
+  function(data,status){
+  	listData =data;//转换为json对象 
+  	
+    $.each(data.data.news_data,function(idx,item){ 
+//	if(idx==0){ 
+//	return true;//同countinue，返回false同break 
+//	} 
+//	alert(item.f_id); 
+//var temsk = $("img.listimg")[0];
+//temsk.attr('src',item.fm_pic);
+//	$('#'+ "listimg" +idx).attr('src',item.fm_pic);
+//	$('#'+"listname"+idx).text(item.f_title);
+//修改标题
+	$("div.mainDiv div:first").children("p").text(item.f_title);
+	//更新封面图
+	$("div.mainDiv div:first").children("div.div-b").children("img.imgreSize").attr('src',item.fm_pic);
+//	$("div.mainDiv div:last img.imgreSize").attr('src',item.fm_pic);
+//	$("div.mainDiv div:last p#listname1").text(item.f_title);
+	//追加模型
+	$("div.mainDiv").append($("div.mainDiv div:first").clone(true));
+//	var divCell = $("div.mainDiv div:last").clone(true);
+//	var divNew = $(divCell).children(div.div-b).children(img).attr("id","listimg"+idx+1);
+	
+//	$("div.mainDiv").append("<div class="div-relative"><div class="div-b"><img id="listimg1" class="imgreSize" src="img/img15.jpg" /></div><div class="div-c"><img width="50" src="img/playGray.png" /></div><p id="listname1">昂山素季打</p></div>");
+	
+	
+	}); 
+	//删除最后一个
+	$("div.mainDiv div:first").empty();
+	$('#'+ "listimg" +idx).css("width", 300);
+	
+  	});
+  	
+//	$("div.div-relative").click(function(){
+//		location.href="htmlSub/VideoPlay.html";//发送txt
+//	}
+  $(".div-relative").click(function(){
+//	alert(listData.data.news_data[0].f_id);
+//if(window.localStorage){
+//  alert('This browser supports localStorage');
+//  localStorage.fid = $('input["name"="name"]').val();
+//}else{
+//  alert('This browser does NOT support localStorage');
+//}
+   location.href="htmlSub/VideoPlay.html";//发送txt
+  });
+ }
+$(document).ready(function(){
+	var listData;
+  
+});
---
+++ js/playVideoJ.js
@@ -0,0 +1,141 @@
+function init() { // Master function, encapsulates all functions
+	
+		
+		var video = document.getElementById("Video1"); 
+		if (video.canPlayType) { // tests that we have HTML5 video support
+		// if successful, display buttons and set up events
+		document.getElementById("buttonbar").style.display = "none"; 
+		document.getElementById("inputField").style.display = "none";//block
+		document.getElementById("videoFile").style.display = "none"; 
+		// helper functions
+		// play video
+		function vidplay(evt) {
+		if (video.src == "") { // inital source load
+		getVideo();
+		}
+		button = evt.target; // get the button id to swap the text based on the state 
+		if (video.paused) { // play the file, and display pause symbol
+		video.play();
+		button.textContent = "||";
+		} else { // pause the file, and display play symbol 
+		video.pause();
+		button.textContent = ">";
+		}
+		}
+		// load video file from input field
+		function getVideo() {
+			
+			//发送请求
+		$.getJSON("http://oa.mgogo.com/chefu/interface/get_two_news_view.php",
+		  {
+		    f_id:"1"//这里不能加冒号哦!
+		  },
+		  function(data,status){
+		  	var listData =data;//转换为json对象 
+//		  	alert(data.data.news_data.f_url);
+		  	
+			var fileURL = data.data.news_data.f_url; // get input field 
+			if (fileURL != "") {
+			video.src = fileURL;
+			video.load(); // if HTML source element is used
+			document.getElementById("play").click(); // start play
+			} else {
+			errMessage("Enter a valid video URL"); // fail silently
+			}
+			
+		 });
+		
+		}
+		  
+		// button helper functions 
+		// skip forward, backward, or restart
+		function setTime(tValue) {
+		// if no video is loaded, this throws an exception 
+		try {
+		if (tValue == 0) {
+		video.currentTime = tValue;
+		}
+		else {
+		video.currentTime += tValue;
+		}
+		 
+		} catch (err) {
+		// errMessage(err) // show exception
+		errMessage("Video content might not be loaded");
+		}
+		}
+		// display an error message 
+		function errMessage(msg) {
+		// displays an error message for 5 seconds then clears it
+		document.getElementById("errorMsg").textContent = msg;
+		setTimeout("document.getElementById('errorMsg').textContent=''", 5000);
+		}
+		// change volume based on incoming value 
+		function setVol(value) {
+		var vol = video.volume;
+		vol += value;
+		// test for range 0 - 1 to avoid exceptions
+		if (vol >= 0 && vol <= 1) {
+		// if valid value, use it
+		video.volume = vol;
+		} else {
+		// otherwise substitute a 0 or 1
+		video.volume = (vol < 0) ? 0 : 1; 
+		}
+		}
+		// button events 
+		// Play
+		document.getElementById("play").addEventListener("click", vidplay, false);
+		// Restart
+		document.getElementById("restart").addEventListener("click", function () {
+		setTime(0);
+		}, false);
+		// Skip backward 10 seconds
+		document.getElementById("rew").addEventListener("click", function () {
+		setTime(-10);
+		}, false);
+		// Skip forward 10 seconds
+		document.getElementById("fwd").addEventListener("click", function () {
+		setTime(10);
+		}, false);
+		// set src == latest video file URL
+		document.getElementById("loadVideo").addEventListener("click", getVideo, false);
+		// fail with message 
+		video.addEventListener("error", function (err) {
+		errMessage(err);
+		}, true);
+		// volume buttons
+		document.getElementById("volDn").addEventListener("click", function () {
+		setVol(-.1); // down by 10%
+		}, false);
+		document.getElementById("volUp").addEventListener("click", function () {
+		setVol(.1); // up by 10%
+		}, false);
+		// playback speed buttons
+		document.getElementById("slower").addEventListener("click", function () {
+		video.playbackRate -= .25;
+		}, false);
+		document.getElementById("faster").addEventListener("click", function () {
+		video.playbackRate += .25;
+		}, false);
+		document.getElementById("normal").addEventListener("click", function () {
+		video.playbackRate = 1;
+		}, false);
+		document.getElementById("mute").addEventListener("click", function (evt) {
+		if (video.muted) {
+		video.muted = false;
+		evt.target.innerHTML = "<img alt='volume on button' src='vol2.png' />"
+		} else {
+		video.muted = true;
+		evt.target.innerHTML = "<img alt='volume off button' src='mute2.png' />"
+		}
+		}, false);
+		} // end of runtime
+		
+		document.getElementById("AppLogo").addEventListener("click", function () {
+		location.href="http://www.anxinchefu.com/interface/share_view.php";
+		}, false);
+		
+		getVideo();
+		
+	}// end of master 
